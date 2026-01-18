904787dc33fccd385a95001eceb5579e6ecb7daf
quralai-baqytnur <isskaktus@gmail.com>
2026-01-18 23:30:16 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { connectDB } = require('./database/mongo');
-const productsRoutes = require('./routes/products');
+const productsRoutes = require('./routes/products'); 
 
 const app = express();
 
@@ -13,20 +13,6 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.send(`
-    <h1>Assignment 3 – Part 1</h1>
-    <p>Backend API with MongoDB</p>
-    <ul>
-      <li><a href="/api/products">GET /api/products</a></li>
-      <li>GET /api/products/:id</li>
-      <li>POST /api/products</li>
-      <li>PUT /api/products/:id</li>
-      <li>DELETE /api/products/:id</li>
-    </ul>
-  `);
-});
 
 app.use('/api/products', productsRoutes);
 
