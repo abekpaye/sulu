616e83421b2907b8886ed4e9f603594fcd31d2a1
quralai-baqytnur <isskaktus@gmail.com>
2026-01-18 23:33:08 +0500

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,42 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
+app.get('/cart', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'cart.html'));
+});
+
+app.get('/tops', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'tops.html'));
+});
+
+app.get('/bottoms', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'bottoms.html'));
+});
+
+app.get('/pyjamas', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'pyjamas.html'));
+});
+
+app.get('/best-sellers', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'best_sellers.html'));
+});
+
+app.get('/chart', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'chart.html'));
+});
+
+app.get('/checkout', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'checkout.html'));
+});
+
 app.use('/api/products', productsRoutes);
 
 app.use('/api', (req, res) => {
