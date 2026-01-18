f19bd7c83dd785a2afb0858f7f5c39bfb4cefb9b
quralai-baqytnur <isskaktus@gmail.com>
2026-01-18 23:09:42 +0500

    Add middleware

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { connectDB } = require('./database/mongo');
-const productsRoutes = require('./routes/products'); 
+const productsRoutes = require('./routes/products');
 
 const app = express();
 
@@ -14,7 +14,25 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Assignment 3 – Part 1</h1>
+    <p>Backend API with MongoDB</p>
+    <ul>
+      <li><a href="/api/products">GET /api/products</a></li>
+      <li>GET /api/products/:id</li>
+      <li>POST /api/products</li>
+      <li>PUT /api/products/:id</li>
+      <li>DELETE /api/products/:id</li>
+    </ul>
+  `);
+});
+
 app.use('/api/products', productsRoutes);
+
+app.use('/api', (req, res) => {
+  res.status(404).json({ error: 'API route not found' });
+});
 
 connectDB().then(() => {
   app.listen(3000, () => {
